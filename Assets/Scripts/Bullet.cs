492b8ee37857041e7e1588a67ac897a83565497c
unknown <aamado785@unab.edu.co>
2022-11-08 21:44:38 -0500

    Primer commit Juego

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 8f;
+    public float lifeDuration = 2f;
+    float lifeTimer;
+    public int attack = 10;
+
+    public bool shooByPlayer;
+    private void OnEnable()
+    {
+        lifeTimer = lifeDuration;
+    }
+    private void Update()
+    {
+        lifeTimer -=Time.deltaTime;
+        if(lifeTimer <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+    private void FixedUpdate()
+    {
+        transform.position += transform.forward * speed * Time.fixedDeltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Bullet choca = " + other.name);
+
+        IDamage damage = other.GetComponent<IDamage>();
+        bool remove = true;
+        if(damage != null)
+        {
+           remove = damage.DoDamage(attack, shooByPlayer);
+        }
+       if(remove == true) gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/StaticValues.cs
@@ -0,0 +1,4 @@
+﻿public static class staticValues
+{
+    public static int winner = -1;
+}
